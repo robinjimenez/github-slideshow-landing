308455a2288d85b92ecd511564471d2c154ddf53
Robin Jiménez Newman <hello@robinjnewman.com>
2020-12-16 23:03:16 +0100

    Create basic slideshow

--- src/app.js
+++ src/app.js
@@ -10,3 +10,19 @@ document.querySelector('#search-form').addEventListener("submit", (e) => {
     e.preventDefault(true);
     github.fetchUser(document.querySelector('#username').value);
 });
+
+document.querySelector('#slideshow').addEventListener("click", (e) => {
+    e.preventDefault(true);
+    if (e.clientX < window.innerWidth / 2) {
+        document.querySelectorAll(".slide").forEach((slide)=>{
+            slide.setAttribute("data-index", parseInt(slide.dataset.index) + 1)
+            if (slide.dataset.index == "4") slide.setAttribute("data-index", "0")
+        })
+    } else {
+        document.querySelectorAll(".slide").forEach((slide)=>{
+            slide.setAttribute("data-index", parseInt(slide.dataset.index) - 1)
+            if (slide.dataset.index == "-1") slide.setAttribute("data-index", "3")
+        })
+    }
+    
+});
